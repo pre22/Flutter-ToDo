1775a4e1fdb7701582f7ee6d082456d761bbab33
Pre22 <pre.chika.22@gmail.com>
2020-09-10 09:24:15 +0100

    chore: Created todo screen

---
+++ lib/model/todo.dart
@@ -0,0 +1,11 @@
+class Todo {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Todo(this._title, this._priority, this._date, [this._description]);
+  Todo.withId(this._id, this._title, this._priority, this._date,
+      [this._description]);
+}
