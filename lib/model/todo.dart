1408d11eceb2d7f1d30c7d24ce1be557a7e13cad
Pre22 <pre.chika.22@gmail.com>
2020-09-11 22:09:30 +0100

    chore: todo.dart code completed

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/scheduler.dart';
+
 class Todo {
   int _id;
   String _title;
@@ -8,4 +10,46 @@ class Todo {
   Todo(this._title, this._priority, this._date, [this._description]);
   Todo.withId(this._id, this._title, this._priority, this._date,
       [this._description]);
+
+  int get id => _id;
+  String get description => _description;
+  String get title => _title;
+  int get priority => _priority;
+  String get date => _date;
+
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      _title = newDescription;
+    }
+  }
+
+  set priority(int newPriority) {
+    if (newPriority > 0 && newPriority <= 3) {
+      _priority = newPriority;
+    }
+  }
+
+  set date(String newDate) {
+    _date = newDate;
+  }
+
+  // Learn how map works
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["title"] = _title;
+    map["description"] = _description;
+    map["date"] = _date;
+    if (_id != null) {
+      map["id"] = _id;
+    }
+    return map;
+  }
+
+  Todo.fromObject(dynamic o) {
+    this._id = o["id"];
+    this._title = o["title"];
+    this._description = o["description"];
+    this._priority = o["priority"];
+    this._date = o["date"];
+  }
 }
